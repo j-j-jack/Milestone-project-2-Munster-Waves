b48939910a5c406afc052fa80248647df2b75b29
unknown <jackosullivan541@gmail.com>
2021-06-19 20:24:41 +0100

    make clare county page with map

---
+++ assets/scripts/clare-map-page.js
@@ -0,0 +1,78 @@
+var clareMap = L.map('clare-map').setView([52.9045, -8.981], 9);
+// code taken from the leaflet provider tool 
+var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 20,
+    ext: 'jpg'
+}).addTo(clareMap);
+
+ var myIcon = L.icon({
+    iconUrl: 'assets/images/surfboard.png', // creating a custom surfboard icon to be used as a marker
+    iconSize: [96, 38],
+    iconAnchor: [48, 19],
+    popupAnchor: [0, 0]
+});
+
+var doolinPointMarker = L.marker([53.009012, -9.412298],{ //coordinates of Doolin Point
+    icon: myIcon, 
+    title: 'Doolin Point',
+    alt: 'surfboard icon showing Doolin Point', 
+    opacity: 1.0
+    });
+doolinPointMarker.addTo(clareMap);
+
+var lahinchMarker = L.marker([52.9338, -9.3499],{ // coordinates of Lahinch
+    icon: myIcon, 
+    title: 'Lahinch',
+    alt: 'surfboard icon showing Lahinch', 
+    opacity: 1.0}
+);
+lahinchMarker.addTo(clareMap);
+
+var spanishPointMarker = L.marker([52.847969, -9.456138],{
+    icon: myIcon, 
+    title: 'Spanish Point',
+    alt: 'surfboard icon showing Spanish Point', 
+    opacity: 1.0}
+);
+spanishPointMarker.addTo(clareMap);
+
+var doughmoreMarker = L.marker([52.806895, -9.499054],{ // coordinates of Doughmore
+    icon: myIcon, 
+    title: 'Doughmore',
+    alt: 'surfboard icon showing Doughmore', 
+    opacity: 1.0}
+);
+doughmoreMarker.addTo(clareMap);
+    
+var kilkeeMarker = L.marker([52.681758, -9.648977],{
+    icon: myIcon, 
+    title: 'Kilkee',
+    alt: 'surfboard icon showing Kilkee', 
+    opacity: 1.0}
+    );
+kilkeeMarker.addTo(clareMap);
+
+//popups
+
+var doolinPointPopup = L.popup();
+doolinPointPopup.setContent("<p></p><a href='spanish-point.html'>View surf forecast for Doolin Point</a>");
+doolinPointMarker.bindPopup(doolinPointPopup);
+
+var lahinchPopup = L.popup();
+lahinchPopup.setContent("<p>Extremely popular surfing town. Good waves and great evenings here.</p><a href='lahinch.html'>View surf forecast for Lahinch</a>");
+lahinchMarker.bindPopup(lahinchPopup);
+
+var spanishPointPopup = L.popup();
+spanishPointPopup.setContent("<p></p><a href='spanish-point.html'>View surf forecast for Spanish Point</a>");
+spanishPointMarker.bindPopup(spanishPointPopup);
+
+var doughmorePopup = L.popup();
+doughmorePopup.setContent("<p></p><a href='doughmore.html'>View surf forecast for Doughmore</a>");
+doughmoreMarker.bindPopup(doughmorePopup);
+
+var kilkeePopup = L.popup();
+kilkeePopup.setContent("<p></p><a href='doughmore.html'>View surf forecast for kilkee</a>");
+kilkeeMarker.bindPopup(kilkeePopup);
