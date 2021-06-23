b02b38fe377527feb66e0e558a2539169c0cd7a3
unknown <jackosullivan541@gmail.com>
2021-06-23 22:27:48 +0100

    add feedback to form on submission

--- assets/scripts/email-script.js
+++ assets/scripts/email-script.js
@@ -4,19 +4,31 @@ the emailjs api is used to send the contents of the form via email to munsterwav
 This is done using the sendForm method as documented on the emailjs website. The input names 
 in the form respond to the variable names used in the template on by my user account on the 
 emailjs website. The sender email is munsterwaves@outlook.com */
+
 document.getElementById('email-form').addEventListener('submit', function(event){
-    let pageForm = document.getElementById('email-form');
     event.preventDefault();
-    emailjs.sendForm('service_qcwv5hy', 'template_mwkm1ac', '#email-form', 'user_G4jZs712zju74E5JnCbeM')
+    let pageForm = document.getElementById('email-form');
+    document.getElementById('form-submission').style.pointerEvents = 'none';
+    emailjs.sendForm(
+        'service_qcwv5hy', 'template_mwkm1ac', '#email-form', 'user_G4jZs712zju74E5JnCbeM')
     .then(function(response) { //protocol to deal with responses as documented on the emailjs website
     console.log('SUCCESS!', response.status, response.text);
+    
+    if(document.getElementById('contact').checked){
+        document.getElementsByClassName('form')[0].innerHTML = 
+        `<h2>Thank you!</h2><p>Your response has been sent.</p>
+        We will send an email to ${pageForm.elements['email'].value}
+        once the shop is open :)`;
+    }
+    else {
+        document.getElementsByClassName('form')[0].innerHTML = 
+        `<h2>Thank you!</h2><p>Your response has been sent.</p>
+        If you want to sign up for the email notification feel free to reload the page
+        and resubmit the form :)`
+    }
+    console.log(pageForm.elements['contact'].value);
     }, function(error) {
        console.log('FAILED...', error);
     })
-/* The response div is shown. The user sees the message 'Thank' you and the following template 
-literals which are created using their answers from the form */
-   console.log(`Your name is: ${pageForm.elements['name'].value}`);
-   console.log(`Your email is: ${pageForm.elements['email'].value}`);
-   console.log(`Your message is ${pageForm.elements['feedback'].value}`);
-   console.log(`Your message is ${pageForm.elements['requests'].value}`);
+    
    });
