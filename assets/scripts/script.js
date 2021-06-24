d491342978018ee8556c486c28a1a908846d653f
unknown <jackosullivan541@gmail.com>
2021-06-24 21:28:38 +0100

    comment common script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,6 +15,43 @@ document.getElementsByClassName('small-screen-navbar-dropdown-curtain')[0].style
 document.getElementsByClassName('company-logo')[0].style.marginTop
    = `${document.getElementsByClassName('small-screen-navbar-container')[0].offsetHeight}px`;
 
+     jQuery.noConflict();
+     jQuery(document).ready(function(){
+        jQuery('.burger').click(function(){ // toggles dropdown on and off
+           jQuery('.small-screen-navbar-dropdown-container').toggle('slow');
+           let smallScreenNavbarDropdownCurtain = document.getElementsByClassName('small-screen-navbar-dropdown-curtain')[0];
+           let smallScreenNavbarContentDisabler = document.getElementsByClassName('small-screen-navbar-content-disabler')[0];
+           if(window.getComputedStyle(smallScreenNavbarDropdownCurtain,
+            null).getPropertyValue('display') == 'none'){
+              smallScreenNavbarDropdownCurtain.style.display = 'block'; // darkens screen
+              smallScreenNavbarContentDisabler.style.pointerEvents = 'none'; //disables clicking
+              smallScreenNavbarDropdownCurtain.style.pointerEvents = 'auto'; //allows clicking to toggle dropdown off again
+           }
+           else{
+            smallScreenNavbarDropdownCurtain.style.display = 'none'; // turns curtain off
+            smallScreenNavbarContentDisabler.style.pointerEvents = 'auto'; // and reenables page clicking
+           }
+            
+      });
+     });  
+
+//code that allows the user to click on the darkened screen to turn the dropdown menu off again
+     jQuery('.small-screen-navbar-dropdown-curtain').click(function(){ 
+      jQuery('.small-screen-navbar-dropdown-container').toggle('slow');
+      let smallScreenNavbarDropdownCurtain = document.getElementsByClassName('small-screen-navbar-dropdown-curtain')[0];
+      let smallScreenNavbarContentDisabler = document.getElementsByClassName('small-screen-navbar-content-disabler')[0];
+           if(window.getComputedStyle(smallScreenNavbarDropdownCurtain,
+            null).getPropertyValue('display') == 'none'){
+              smallScreenNavbarDropdownCurtain.style.display = 'block';
+              smallScreenNavbarContentDisabler.style.pointerEvents = 'none';
+           }
+           else{
+            smallScreenNavbarDropdownCurtain.style.display = 'none';
+            smallScreenNavbarContentDisabler.style.pointerEvents = 'auto';
+           }
+     });
+
+//this code works the same as the scroll function but for when the screen is resized.
 window.addEventListener('resize', function(){
    document.getElementsByClassName('company-logo')[0].style.marginTop
    = `${document.getElementsByClassName('small-screen-navbar-container')[0].offsetHeight}px`;
@@ -31,45 +68,11 @@ window.addEventListener('resize', function(){
     = `${aHeight - window.scrollY}px`;
 }
 );
-
-
-     jQuery.noConflict();
-     jQuery(document).ready(function(){
-        jQuery('.burger').click(function(){
-           jQuery('.small-screen-navbar-dropdown-container').toggle('slow');
-           let smallScreenNavbarDropdownCurtain = document.getElementsByClassName('small-screen-navbar-dropdown-curtain')[0];
-           let smallScreenNavbarContentDisabler = document.getElementsByClassName('small-screen-navbar-content-disabler')[0];
-           if(window.getComputedStyle(smallScreenNavbarDropdownCurtain,
-            null).getPropertyValue('display') == 'none'){
-              smallScreenNavbarDropdownCurtain.style.display = 'block';
-              smallScreenNavbarContentDisabler.style.pointerEvents = 'none';
-              smallScreenNavbarDropdownCurtain.style.pointerEvents = 'auto';
-           }
-           else{
-            smallScreenNavbarDropdownCurtain.style.display = 'none';
-            smallScreenNavbarContentDisabler.style.pointerEvents = 'auto';
-           }
-            
-      });
-     });  
-     jQuery('.small-screen-navbar-dropdown-curtain').click(function(){
-      jQuery('.small-screen-navbar-dropdown-container').toggle('slow');
-      let smallScreenNavbarDropdownCurtain = document.getElementsByClassName('small-screen-navbar-dropdown-curtain')[0];
-      let smallScreenNavbarContentDisabler = document.getElementsByClassName('small-screen-navbar-content-disabler')[0];
-           if(window.getComputedStyle(smallScreenNavbarDropdownCurtain,
-            null).getPropertyValue('display') == 'none'){
-              smallScreenNavbarDropdownCurtain.style.display = 'block';
-              smallScreenNavbarContentDisabler.style.pointerEvents = 'none';
-           }
-           else{
-            smallScreenNavbarDropdownCurtain.style.display = 'none';
-            smallScreenNavbarContentDisabler.style.pointerEvents = 'auto';
-           }
-     });
-
      
-
-     document.addEventListener('scroll', function(){
+/* the code below makes the small screen nav menu appear under the announcement bar in its initial position
+and as the user scrolls it calculates the postion of the annoucement bar and keeps it at the end of it.
+when the announcement bar disappears the navbar stays at the top of the screen */
+     document.addEventListener('scroll', function(){ 
       aHeight = document.getElementsByClassName('announcement-bar')[0].offsetHeight;
      if ((aHeight-window.scrollY) > 0){
         document.getElementsByClassName('small-screen-navbar-container')[0].style.top = `${aHeight - window.scrollY}px`;
@@ -84,7 +87,8 @@ window.addEventListener('resize', function(){
    });
      
   
-
+/* the code below was a convenient way to make the full divs clickable rather than using the anchor
+tags in the html */
   let clareNav = document.getElementsByClassName('clare-nav'); 
   clareNav[0].addEventListener('click', function(){
               window.location.href = "clare.html";
