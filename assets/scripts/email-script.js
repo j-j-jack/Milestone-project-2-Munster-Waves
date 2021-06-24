d8f221b9e6b616e5841db2b66f2a3d21691c8b87
unknown <jackosullivan541@gmail.com>
2021-06-24 21:16:22 +0100

    comment email script

--- assets/scripts/email-script.js
+++ assets/scripts/email-script.js
@@ -3,7 +3,7 @@
 the emailjs api is used to send the contents of the form via email to munsterwaves@tutanota.com. 
 This is done using the sendForm method as documented on the emailjs website. The input names 
 in the form respond to the variable names used in the template on by my user account on the 
-emailjs website. The sender email is munsterwaves@outlook.com */
+emailjs website. The sender email is jackosullivan541@gmail.com */
 
 document.getElementById('email-form').addEventListener('submit', function(event){
     event.preventDefault();
@@ -14,15 +14,15 @@ document.getElementById('email-form').addEventListener('submit', function(event)
     .then(function(response) { //protocol to deal with responses as documented on the emailjs website
     console.log('SUCCESS!', response.status, response.text);
     
-    if(document.getElementById('contact').checked){
-        document.getElementsByClassName('form')[0].innerHTML = 
+    if(document.getElementById('contact').checked){ //checks if the user wants to be emailed
+        document.getElementsByClassName('form')[0].innerHTML = // replaces the form with the html below
         `<h2>Thank you!</h2><p>Your response has been sent.</p>
         <p>We will send an email to ${pageForm.elements['email'].value} once the shop opensgit  :)</p> 
         <p>click <a href ="index.html">here to go back to home page</a></p>
         `;
     }
     else {
-        document.getElementsByClassName('form')[0].innerHTML = 
+        document.getElementsByClassName('form')[0].innerHTML = // otherwise gives the user option to resubmit
         `<h2>Thank you!</h2><p>Your response has been sent.</p>
         <p><a href="signup.html">reload the page here</a> to resubmit the form 
         and register your email <br>
@@ -30,9 +30,9 @@ document.getElementById('email-form').addEventListener('submit', function(event)
         `;
     }
     console.log(pageForm.elements['contact'].value);
-    }, function(error) {
+    }, function(error) { // if there is an error the user can click the link to resubmit or return home
        document.getElementsByClassName('form')[0].innerHTML = 
-       `<h2>Oops... Something went wrong</h2><p>Your response wasn't sent</p>
+       `<h2>Oops... Something went wrong</h2><p>Your response wasn't sent</p> 
        <a href="signup.html">reload the page here</a> to try again <br>
        or click <a href ="index.html">here to go back to home page</a> :)`
     })
